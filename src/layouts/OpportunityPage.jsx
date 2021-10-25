a27bf18ab3aeb64e0bd39842a7331549eb102984
emanuel.pistara <emanuel.pistara@accenture.com>
2021-10-25 17:23:16 +0200

    hero-opportunita #comment complete

--- src/layouts/OpportunityPage.jsx
+++ src/layouts/OpportunityPage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import content from '../../contents/home-page/home.yml';
+import opportunityContent from '../../contents/opportunity-page/opportunity.yml';
 import { BeneficiariesSection } from './opportunity/BeneficiariesSection';
 import { SupportSection } from './faq/SupportSection';
 import { HeroImageBackground } from '../components/hero/HeroImageBackground';
 
 const { heroDigital, support } = content;
+const { heroOpportunity } = opportunityContent;
 
 export const OpportunityPage = () => (
   <>
     <HeroImageBackground
-      title={heroDigital.title}
-      body={heroDigital.body}
+      title={heroOpportunity.title}
+      body={heroOpportunity.body}
       image="opportunity-hero.png"
-      theme="bg-white"
+      theme="bg-white flex-xs-column-reverse"
     />
     <BeneficiariesSection />
     <SupportSection
