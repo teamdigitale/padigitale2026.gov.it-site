3eabfb7f32ee8f0a15bcb403ec03f494b8d58ca1
lollomag <47573951+lollomag@users.noreply.github.com>
2021-11-02 17:29:19 +0100

    Update src/layouts/come-funziona/Beneficiaries.jsx
    Co-authored-by: Alessandro Sebastiani <sebbalex@users.noreply.github.com>

--- src/layouts/come-funziona/Beneficiaries.jsx
+++ src/layouts/come-funziona/Beneficiaries.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { Container, Row, Col } from 'design-react-kit';
 import { Link } from 'gatsby';
