f009074c95c9c716b452ce0e70a1a7aac1814500
emanuel.pistara <emanuel.pistara@accenture.com>
2021-11-03 10:13:15 +0100

    hero-come-funziona #comment WIP

--- src/layouts/ComeFunzionaPage.jsx
+++ src/layouts/ComeFunzionaPage.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import content from '../../contents/come-funziona/come-funziona.yml';
 import { SEO } from '../components/SEO';
 import { Involved } from './come-funziona/Involved';
+import { HeroHowItWorks } from '../components/hero/HeroHowItWorks';
 
-export const ComeFunzionaPage = () => (
-  <>
-    <SEO title={content.title} />
-    <div className="sr-only">
-      <h1>{content.name}</h1>
-    </div>
-    <Involved title={content.involved.title} category={content.involved.category} cards={content.involved.cards} />
-  </>
-);
+export const ComeFunzionaPage = () => {
+  const { title, name, involved, hero } = content;
+  return (
+    <>
+      <SEO title={title} />
+      <div className="sr-only">
+        <h1>{name}</h1>
+      </div>
+      <HeroHowItWorks
+        title={hero.title}
+        body={hero.body}
+        theme="bg-white"
+        smallText="true"
+        image="come-funziona-hero.svg"
+        list={hero.list}
+      />
+      <Involved
+        title={involved.title}
+        category={involved.category}
+        cards={involved.cards}
+      />
+    </>
+  );
+};
