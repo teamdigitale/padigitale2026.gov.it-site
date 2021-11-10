95ec8c74fa87813f13dd2da49acd926d78230cf5
lollomag <47573951+lollomag@users.noreply.github.com>
2021-11-10 14:07:32 +0100

    fix: issue #137 #139 #140 (#142)
    Co-authored-by: lollomag <“lorenzo.cadente@yahoo.it”>

--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -110,7 +110,7 @@ export const HeroImage = ({
               </ExternalLink>
             }
             {secondButtonLabel ? (
-              <Link className="btn text-uppercase mx-4 ml-lg-0 my-3 my-md-0 btn-outline-primary" to="/come-funziona" onClick={() => dispatch({type: 'SET:SECTION_ID', payload: {sectionId: 'beneficiari'}})}>
+              <Link className="btn text-uppercase mx-4 ml-lg-0 my-3 my-md-0 btn-outline-primary" to="/come-funziona" onClick={() => dispatch({type: 'SET:HOW_SECTION_ID', payload: {howId: 'beneficiari'}})}>
                 {secondButtonLabel}
               </Link>
             ) : (
