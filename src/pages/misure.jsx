534a125cad4751c79bfc3902e7330be88f6d16b4
emanuel.pistara <emanuel.pistara@accenture.com>
2022-02-18 16:49:14 +0100

    fix open accordion from url

--- src/pages/misure.jsx
+++ src/pages/misure.jsx
@@ -2,19 +2,33 @@ import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { OpportunityPage } from '../layouts/OpportunityPage';
 import { GlobalStateContext } from '../context/globalContext';
+import { selectBeneficiaries } from '../../contents/opportunity-page/opportunity.yml';
 
 const Page = ({ location }) => {
   const [{ sectionId }] = useContext(GlobalStateContext);
-  const [filter] = useState(null);
+  const [filter, setFilter] = useState(null);
   const [, dispatch] = useContext(GlobalStateContext);
   const currentHash = location.hash?.split('#')[1];
 
-  if (!sectionId) {
-    dispatch({
-      type: 'SET:SECTION_OPPORTUNITY_ID',
-      payload: { sectionId: currentHash },
-    });
-  }
+  const hashFromSelect = selectBeneficiaries.find((option) => currentHash === option.value);
+  useEffect(() => {
+    if (!hashFromSelect) {
+      if (!sectionId) {
+        dispatch({
+          type: 'SET:SECTION_OPPORTUNITY_ID',
+          payload: { sectionId: currentHash },
+        });
+      }
+    } else {
+      const filter = {
+        label: '',
+        value: '',
+      };
+      filter.label = hashFromSelect.label;
+      filter.value = currentHash;
+      setFilter(filter);
+    }
+  }, [hashFromSelect, sectionId, dispatch, currentHash]);
 
   useEffect(() => {
     dispatch({ type: 'SET:ACTIVE_HEADER', payload: { activeItem: 'misure' } });
