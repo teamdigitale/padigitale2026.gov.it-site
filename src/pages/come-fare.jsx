7cae606f3062b647507b68d5c55a4d8d3c56e06e
emnlpstr <emanuelpistara@gmail.com>
2022-03-18 14:31:40 +0100

    fix lint

--- src/pages/come-fare.jsx
+++ src/pages/come-fare.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { ComeFarePage } from '../layouts/ComeFarePage';
 
-const Page = () => {
-  return <ComeFarePage />;
-};
+const Page = () => <ComeFarePage />;
 export default Page;
