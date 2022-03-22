b58292da40e9f7e647f6fe46aa12689a2b9c85e0
Fladiir <bianchi.matteo97@gmail.com>
2022-03-22 21:57:37 +0100

    feat: come partecipare page (#322)
    * come-partecipare progress
    
    * come partecipare page progress
    
    * wip
    
    * come-partecipare progress
    
    * progress
    
    * page come fare
    
    * progress
    
    * progress
    
    * top profilo page
    
    * fix lint
    
    * iframe title
    
    * some fixes
    
    * lint fixes
    
    * priettier fixes
    
    * priettier fixes
    
    * fixes
    
    * fixes
    
    * fix lint
    
    * fix lint
    
    * fix
    
    * fix seo
    
    * progress graphic
    
    * crea-profilo progress
    
    * fix lint
    
    * removed comment blocks
    
    * fixed accordion closed on mobile and timeline
    
    * remove comment block
    
    * fixed scroll
    
    * clean
    
    * recaptcha
    
    Co-authored-by: emnlpstr <emanuelpistara@gmail.com>
    Co-authored-by: RIDP9CT <daniela.vallo.external@snam.it>

---
+++ src/components/HeroVideo.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyle = createUseStyles({
+  heroVideo: {
+    display: 'flex',
+    padding: '82px 0 82px 1.5rem',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    minHeight: '412px',
+    '@media (max-width: 991px)': {
+      flexDirection: 'column',
+      padding: '82px 0',
+    },
+  },
+  info: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& h4': {
+      fontSize: '1.688rem',
+      fontWeight: '700',
+    },
+  },
+  videoContainer: {
+    flexBasis: '55%',
+    height: '100%',
+    width: '100%',
+    aspectRatio: '16 / 9',
+  },
+});
+
+export const HeroVideo = () => {
+  const classes = useStyle();
+
+  return (
+    <>
+      <div className={classes.heroVideo}>
+        <div className={classes.info}>
+          <h4>Guarda il video dedicato</h4>
+          <p>
+            Scopri come creare il profilo della tua amministrazione all&apos;interno della piattaforma PA digitale 2026.
+          </p>
+        </div>
+        <div className={classes.videoContainer}>
+          <iframe
+            src="https://player.vimeo.com/video/142548770?h=4205667b4f"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+            title="video-container"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/come-partecipare.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ComeParteciparePage } from '../layouts/ComeParteciparePage';
+
+const Page = () => <ComeParteciparePage />;
+export default Page;
