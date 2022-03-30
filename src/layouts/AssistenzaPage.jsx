46cda78fecc2d936e6669d38be436809063bcf1b
RIDP9CT <daniela.vallo.external@snam.it>
2022-03-30 15:24:48 +0200

    fix lint

--- src/layouts/AssistenzaPage.jsx
+++ src/layouts/AssistenzaPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable sonarjs/cognitive-complexity */
 import React, { useState, useEffect } from 'react';
