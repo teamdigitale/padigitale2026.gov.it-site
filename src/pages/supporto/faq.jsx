7ec6dc403e7dcfbb7bda00f331c1f2982a47d84c
Emanuel Pistarà <52462767+emnlpstr@users.noreply.github.com>
2022-04-01 12:32:06 +0200

    feat: rename faq to domande-frequenti keeping redirection (#354)
    * fix ui
    
    * create redirect
    
    Co-authored-by: emnlpstr <emanuelpistara@gmail.com>

--- src/pages/supporto/faq.jsx
+++ src/pages/supporto/faq.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useContext } from 'react';
-import { FaqPage } from '../../layouts/FaqPage';
-import { GlobalStateContext } from '../../context/globalContext';
-
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 const Page = () => {
-  const [, dispatch] = useContext(GlobalStateContext);
-
   useEffect(() => {
-    dispatch({ type: 'SET:ACTIVE_HEADER', payload: { activeItem: 'supporto' } });
-    return () => {
-      dispatch({ type: 'SET:ACTIVE_HEADER' });
-    };
-  }, [dispatch]);
-  return <FaqPage />;
+    navigate('/supporto/domande-frequenti');
+  }, []);
+  return <></>;
 };
 export default Page;
