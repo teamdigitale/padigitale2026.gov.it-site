9e87799b352756e401b69b726587eafc21ee2817
Fladiir <bianchi.matteo97@gmail.com>
2022-04-01 19:48:07 +0200

    feat: modalita hero (#356)
    * hero mode added
    
    * refactor
    
    * fix page path
    
    Co-authored-by: RIDP9CT <daniela.vallo.external@snam.it>

--- src/layouts/SolutionStandard.jsx
+++ src/layouts/SolutionStandard.jsx
@@ -4,11 +4,10 @@ import { Breadcrumb, BreadcrumbItem, Row, Col } from 'design-react-kit';
 import content from '../../contents/iniziativa/soluzione-standard.yml';
 import { SEO } from '../components/SEO';
 import seo from '../../contents/seo.yml';
-import contentHowItWorks from '../../contents/iniziativa/iniziativa.yml';
 import { GlobalStateContext } from '../context/globalContext';
 import { HeroSolutions } from '../components/hero/HeroSolutions';
 import { TimelineVertical } from '../components/TimelineVertical';
-import { ProjectsCards } from '../components/ProjectsCards';
+import { HeroMode } from '../components/hero/HeroMode';
 
 const { title: seoTitle, description: seoDescription } = seo.standardSolutionPage;
 
@@ -47,8 +46,7 @@ const useStyles = createUseStyles({
 export const SolutionStandard = () => {
   const classes = useStyles();
 
-  const { hero, verticalTimeline } = content;
-  const { projectsCardsItem } = contentHowItWorks;
+  const { hero, verticalTimeline, heroMode } = content;
   const [, dispatch] = useContext(GlobalStateContext);
 
   useEffect(() => {
@@ -92,7 +90,19 @@ export const SolutionStandard = () => {
         smallText="true"
       />
       <TimelineVertical item={verticalTimeline} />
-      <ProjectsCards item={projectsCardsItem} />
+      <HeroMode
+        ctaContainer={true}
+        category={heroMode.category}
+        title={heroMode.title}
+        body={heroMode.body}
+        imageUrl="/assets/Soluzioni_Standard.svg"
+        imageAlt=""
+        firstInternal={true}
+        firstButtonHref={heroMode.link}
+        firstButtonLabel={heroMode.btnLabel}
+        firstButtonAriaLabel={heroMode.btnAria}
+        heroTitleId="home-hero-title-candidature"
+      />
     </>
   );
 };
