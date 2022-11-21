0f9f50f324e23b2d37b8d5dbdfd4553584ec8bd1
Denis Marini <denis.marini@gmail.com>
2022-11-21 18:07:43 +0100

    Update opendata.jsx

--- src/pages/opendata.jsx
+++ src/pages/opendata.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { OpendataPage } from '../layouts/OpendataPage';
 import { GlobalStateContext } from '../context/globalContext';
 
 const Page = () => {
+    const [, dispatch] = useContext(GlobalStateContext);
 
-const [, dispatch] = useContext(GlobalStateContext);
-
-  useEffect(() => {
-    dispatch({ type: 'SET:ACTIVE_HEADER', payload: { activeItem: 'opendata' } });
-    return () => {
-      dispatch({ type: 'SET:ACTIVE_HEADER' });
-    };
-  }, [dispatch]);
-
+    useEffect(() => {
+        dispatch({ type: 'SET:ACTIVE_HEADER', payload: { activeItem: 'opendata' } });
+        return () => {
+            dispatch({ type: 'SET:ACTIVE_HEADER' });
+        };
+    }, [dispatch]);
   return <OpendataPage title="Opendata" />;
 }
-
 export default Page;
