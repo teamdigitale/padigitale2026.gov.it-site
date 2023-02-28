5a3e6628af5936da65ebbc3da423c3415dd2dbf5
Renato <renato.taffi@innovamarine.ch>
2023-02-28 17:57:52 +0100

    Aggiunta sezione "Mat e Risorse"

--- src/layouts/SupportPage.jsx
+++ src/layouts/SupportPage.jsx
@@ -6,13 +6,25 @@ import seo from '../../contents/seo.yml';
 import { HeroSupport } from './support/Hero';
 import { FAQPreview } from './support/FAQPreview';
 import { Assistance } from './support/Assistance';
+import { KeypointSection } from './come-fare/KeypointSection';
 
 const { title: seoTitle, description: seoDescription } = seo.supportPage;
 
 const {
   mainHero: { title, description },
 } = content;
-
+const matAndResource = {
+  number: '01',
+  title: 'Materiali e risorse',
+  text: "Esplora tutti gli strumenti per favorire l'implementazione delle misure PNRR per la digitalizzazione della PA",
+  image: 'MaterialieRisorse.svg',
+  button: true,
+  buttonLabel: 'ESPLORA',
+  linkTo: '/supporto/materiali-e-risorse',
+  lBlue: true,
+  ariaLabel: 'Esplora materiali e risorse',
+  reverse: true,
+};
 export const SupportPage = () => {
   useEffect(() => {
     announce('Pagina caricata ' + content.name);
@@ -25,6 +37,7 @@ export const SupportPage = () => {
       <h2 className="sr-only">Domande frequenti</h2>
       <FAQPreview />
       <Assistance />
+      <KeypointSection item={matAndResource} />
     </>
   );
 };
