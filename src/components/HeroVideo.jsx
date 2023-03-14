ef996b90bcd8f55cea27dc82251e60a293756361
Renato <renato.taffi@innovamarine.ch>
2023-03-14 11:19:03 +0100

    Fix container "videoTranslation" per testi lunghi

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -37,6 +37,9 @@ const useStyle = createUseStyles({
       paddingBottom: '0',
     },
   },
+  videoTraslation: {
+    maxWidth: '45em',
+  },
   info: {
     display: 'flex',
     flexDirection: 'column',
@@ -81,7 +84,9 @@ export const HeroVideo = ({ src, description, videoText }) => {
               Trascrizione del video
             </AccordionHeader>
             <AccordionBody active={collapseOpen}>
-              <p dangerouslySetInnerHTML={{ __html: videoText }} />
+              <p className={classes.videoTraslation}>
+                <span dangerouslySetInnerHTML={{ __html: videoText }} />
+              </p>
             </AccordionBody>
           </Accordion>
         ) : (
