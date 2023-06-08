4ee5fe79b536303ee8285e2d4802b463756cf348
Renato <renato.taffi@innovamarine.ch>
2023-06-08 17:02:20 +0200

    Faq string Capitalized, Reload view fixed

---
+++ src/pages/supporto/domande-frequenti-new.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useContext } from 'react';
+import { FaqPageNew } from '../../layouts/FaqPageNew';
+import { GlobalStateContext } from '../../context/globalContext';
+
+const Page = () => {
+  const [, dispatch] = useContext(GlobalStateContext);
+
+  useEffect(() => {
+    dispatch({ type: 'SET:ACTIVE_HEADER', payload: { activeItem: 'supporto' } });
+    return () => {
+      dispatch({ type: 'SET:ACTIVE_HEADER' });
+    };
+  }, [dispatch]);
+  return <FaqPageNew />;
+};
+export default Page;
