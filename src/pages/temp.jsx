a03bd40ee6e793db0c54fd5df8efc0f1e389996a
Renato <renato.taffi@innovamarine.ch>
2023-06-09 13:24:57 +0200

    unnecessaries files removed

--- src/pages/temp.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { NotFoundPage } from '../layouts/NotFoundPage';
-import { TempPage } from '../layouts/TempPage';
-const Page = () => <TempPage />;
-export default Page;
