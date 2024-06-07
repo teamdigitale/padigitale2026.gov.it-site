7cf18dff7def202537cea4c5e638a6aaeb9d6b9d
Nicola Squartini <tensor5@gmail.com>
2024-06-07 12:06:04 +0200

    feat: add FSE banner

---
+++ src/components/FseBox.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Container } from 'design-react-kit';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  outerBox: {
+    border: '1px solid #06C',
+    background: '#FFF',
+    display: 'flex',
+    padding: '24px',
+    marginBottom: '48px',
+  },
+  text: {
+    fontFamily: 'Titillium Web',
+    lineHeight: '24px',
+  },
+  logo: {
+    backgroundColor: '#06c',
+    marginRight: '24px',
+    width: '200px',
+  },
+  title: {
+    color: '#1A1A1A',
+    fontSize: '18px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    marginBottom: '8px',
+  },
+  description: {
+    color: '#2F475E',
+    fontSize: '16px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    marginBottom: '16px',
+  },
+  link: {
+    fontSize: '16px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    color: '#06c',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#06c',
+    },
+  },
+});
+
+export function FseBox() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <div className={classes.outerBox}>
+        <div className={classes.logo}></div>
+        <div className={classes.text}>
+          <p className={classes.title}>
+            È disponibile il cruscotto di monitoraggio del Fascicolo Sanitario Elettronico
+          </p>
+
+          <p className={classes.description}>
+            Controlla l’avanzamento degli obiettivi del PNRR legati all’ambito sanità e consulta gli indicatori
+            nazionali, regionali e per azienda sanitaria.
+          </p>
+          <a className={classes.link} href="https://monitoring.fse.salute.gov.it" rel="noreferrer" target="_blank">
+            Accedi al cruscotto
+          </a>
+        </div>
+      </div>
+    </Container>
+  );
+}
